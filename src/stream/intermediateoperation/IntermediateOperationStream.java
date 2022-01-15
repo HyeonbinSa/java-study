349d9f5e1545020b5d384529110a6db9b831eafa
HyeonbinSa <shb1833@gmail.com>
2022-01-15 13:42:59 +0900

    [Stream] Stream 중간 연산 학습 - skip(),  litmit(), distinct(), fiter(), sorted()

--- src/stream/intermediateoperation/IntermediateOperationStream.java
+++ src/stream/intermediateoperation/IntermediateOperationStream.java
@@ -1,7 +1,67 @@
 package stream.intermediateoperation;
 
+import sun.jvm.hotspot.utilities.IntArray;
+
+import java.util.Comparator;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class IntermediateOperationStream {
     public static void main(String[] args) {
-        System.out.println("say hi");
+        skip();
+        limit();
+        skipAndLimit();
+        distinct();
+        filter();
+        sorted();
+    }
+
+    private static void skip() {
+        System.out.print("중간 연산 skip() : ");
+        IntStream intStream = IntStream.rangeClosed(1, 10);
+        intStream.skip(5).forEach(System.out::print);
+        System.out.println();
+    }
+
+    private static void limit() {
+        System.out.print("중간 연산 limit() : ");
+        IntStream intStream = IntStream.rangeClosed(1, 10);
+        intStream.limit(7).forEach(System.out::print);
+        System.out.println();
+    }
+
+    private static void skipAndLimit() {
+        System.out.print("중간 연산 skip().limit() : ");
+        IntStream intStream = IntStream.rangeClosed(1, 10);
+        intStream.skip(3).limit(5).forEach(System.out::print);
+        System.out.println();
+    }
+
+    private static void distinct() {
+        System.out.print("중간 연산 distinct() : ");
+        IntStream intStream = IntStream.of(1, 1, 1, 2, 2, 2, 3, 4, 3, 2, 5, 6);
+        intStream.distinct().forEach(System.out::print);
+        System.out.println();
+    }
+
+    private static void filter() {
+        System.out.print("중간 연산 filter() : ");
+        IntStream intStream = IntStream.rangeClosed(1, 10);
+        intStream.filter(n -> n % 2 == 0).forEach(System.out::print);
+        System.out.println();
+    }
+
+    private static void sorted() {
+        // 기본 정렬 : 인자값 없음.
+        System.out.print("중간 연산 sorted() : ");
+        Stream<String> strStream = Stream.of("Z", "apple", "Google", "wed", "Downer", "cherry", "BANANA");
+        strStream.sorted().forEach(str -> System.out.print(str + " "));
+        System.out.println();
+
+        // 정렬 기준 존재
+        System.out.print("중간 연산 sorted(comparator) : ");
+        strStream = Stream.of("Z", "apple", "Google", "wed", "Downer", "cherry", "BANANA");
+        strStream.sorted(Comparator.comparing(String::length)).forEach(str -> System.out.print(str + " "));
+        System.out.println();
     }
 }
